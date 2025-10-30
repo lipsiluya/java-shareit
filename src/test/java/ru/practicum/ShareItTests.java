934aabd9a23f79477dae154ddbcc9dd6486f766d
Maxim Telegin <maximtelegin@MacBook-Pro-Maxim.local>
2025-10-30 04:07:25 +0300

    f

---
+++ src/test/java/ru/practicum/ShareItTests.java
@@ -0,0 +1,12 @@
+package ru.practicum;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ShareItTests {
+
+	@Test
+	void simpleTest() {
+		assertTrue(true, "Basic test should pass");
+	}
+}
