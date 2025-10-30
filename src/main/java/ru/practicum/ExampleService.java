7a020172aa0496d2706034fba43f5c2de4406f6f
Maxim Telegin <maximtelegin@MacBook-Pro-Maxim.local>
2025-10-30 03:45:28 +0300

    f

--- src/main/java/ru/practicum/ExampleService.java
+++ src/main/java/ru/practicum/ExampleService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+//ff
 @Slf4j
 @Service
 public class ExampleService {
